019ee9bc21c71a2ba6eb3df1093a6055622f7070
Allando <alla2634@edu.easj.dk>
2017-03-03 13:49:20 +0100

    random

--- Assets/scripts/EnemyWaveScript.cs
+++ Assets/scripts/EnemyWaveScript.cs
@@ -32,7 +32,7 @@ public class EnemyWaveScript : MonoBehaviour
                         for (int i = 0; i < A.spawnCount; i++)
                         {
                             Debug.Log("Wave");
-                            Vector3 spawnPosition = new Vector3(10,0,0);
+                            Vector3 spawnPosition = new Vector3(10, Random.Range(-5, 5), 0);
                             Quaternion spawnRotation = Quaternion.identity;
                             Instantiate(A.prefab, spawnPosition, spawnRotation);
                         }
